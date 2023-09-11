8cb8862003b8fba9cd85c035532f424e941ef85c
Renata Moura <133124737+remourab@users.noreply.github.com>
2023-09-11 20:59:09 -0300

    bike.ts

---
+++ bike.ts
@@ -0,0 +1,14 @@
+export class Bike {
+    constructor(
+        public name: string,
+        public type: string,
+        public bodySize: number,
+        public maxLoad: number,
+        public rate: number,
+        public description: string,
+        public ratings: number,
+        public disponivel: boolean,
+        public imageUrls: string[],
+        public id?: string
+    ) {}
+}
